5c8f9e9fc45ca3e07e656ecec7b129ea2f12e3b2
FredericoWP <frederico.wallace@yahoo.com>
2022-11-23 19:48:04 -0300

    ...

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,5 @@
-import 'package:animation_expansive/controller.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:animation_expansive/controller/controller.dart';
+import 'package:animation_expansive/widget/container_expansive.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -33,62 +33,6 @@ class _MyHomePageState extends State<MyHomePage>
             return ContainerExpansive(controller: controller, index: index);
           },
         ),
-      ),
-    );
-  }
-}
-
-class ContainerExpansive extends StatefulWidget {
-  const ContainerExpansive(
-      {super.key, required this.controller, required this.index});
-  final ControllerAnimation controller;
-  final int index;
-
-  @override
-  State<ContainerExpansive> createState() => _ContainerExpansiveState();
-}
-
-class _ContainerExpansiveState extends State<ContainerExpansive> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          ClipRRect(
-            child: Container(
-              color: widget.controller.getBgColor(widget.index),
-              child: ListTile(
-                onTap: () => widget.controller.execute(widget.index),
-                textColor: widget.controller.getTxtColor(widget.index),
-                title: Text(
-                  "Titulo ${widget.index < 10 ? '0${widget.index}' : widget.index}",
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.w600),
-                ),
-                trailing: Transform.rotate(
-                  angle: widget.controller.getRotation(widget.index),
-                  child: const Icon(CupertinoIcons.arrow_down),
-                ),
-              ),
-            ),
-          ),
-          Align(
-            heightFactor: widget.controller.factorHeight(widget.index),
-            alignment: Alignment.topCenter,
-            child: Container(
-              margin: const EdgeInsets.all(15),
-              child: Column(
-                children: const [
-                  FlutterLogo(),
-                  Text(
-                      "Flutterando is the largest Flutter community in Brazil. Think about Flutter, think about Flutterando. Be part of our community!"),
-                  Divider(),
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
